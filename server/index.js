f25ca6294647ff1a29d59618b3d73ab003d01d9f
Aditya Dhanraj <opsingh861@gmail.com>
2024-03-06 22:27:17 +0530

    client intialized

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-import 
+import Express from "express";
+
+const app = new Express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
