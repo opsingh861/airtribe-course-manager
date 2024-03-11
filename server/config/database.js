cc3021fbbf6499ad83dd68c4f625115df5380f0a
Aditya Dhanraj <opsingh861@gmail.com>
2024-03-11 23:48:35 +0530

    fixed containerization issue

--- server/config/database.js
+++ server/config/database.js
@@ -1,14 +1,21 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("airtribe", "root", "root", { dialect: "mysql", host: "db"});
+const sequelize = new Sequelize("airtribe", "root", "root", {
+  dialect: "mysql",
+  host: "db"
+});
+
 
 (async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+    // Synchronize models with the database
+    await sequelize.sync({ force: false });
+    console.log('Models have been synchronized with the database.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 })();
 
 export default sequelize;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 import Express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import sequelize from "./config/database.js"
+import sequelize from "./config/database.js";
 import Course from "./models/Course.js";
-import Instructor from "./models/Instructor.js"
+import Instructor from "./models/Instructor.js";
 import Lead from "./models/Lead.js";
 import Comment from "./models/Comment.js";
 import course from "./routes/course.js";
@@ -15,24 +15,35 @@ app.use(cors());
 dotenv.config();
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+  res.send("Hello World");
 });
 
 app.use("/api/course", course);
 app.use("/api/instructor", instructor);
 
+const connectWithRetry = async () => {
+  let retries = 5;
+  while (retries) {
+    try {
+      await sequelize.authenticate();
+      console.log("Connection has been established successfully.");
+      break;
+    } catch (error) {
+      console.error("Unable to connect to the database:", error);
+      console.log(`Retrying in 5 seconds... (${retries} attempts left)`);
+      retries--;
+      await new Promise((res) => setTimeout(res, 5000));
+    }
+  }
+};
 
-(async () => {
+connectWithRetry().then(() => {
+  app.listen(8080, () => {
     sequelize.sync({ force: false }).then(() => {
-        console.log("Database Synced Successfully");
+      console.log("Database Synced Successfully");
     }).catch((err) => {
-        console.log(err);
+      console.log(err);
     });
-})();
-
-
-
-app.listen(8080, () => {
-
     console.log("Server is running on port 8080");
+  });
 });
